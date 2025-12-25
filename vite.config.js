a9ee07bbd0d79527b505137412e3c66119182b85
Steve Garrett <steve.garrett.au@gmail.com>
2025-12-25 12:14:47 +0000

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/sliding-tiles/',
   plugins: [
     react(),
     VitePWA({
@@ -16,8 +17,8 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: '/sliding-tiles/',
+        start_url: '/sliding-tiles/',
         icons: [
           {
             src: 'icon-192.svg',
